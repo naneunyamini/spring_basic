adccc2b93ddaf4f77b0cd7971742c1552af84da4
naneunyamini <minp4028@gmail.com>
2025-08-21 12:16:30 +0900

    객체 지향 원리 적용

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -9,6 +11,26 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
+    // 리팩토링하기
+    public MemberService memberService(){
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public OrderService orderService(){
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
+
+
+    /*
     public MemberService memberService(){
         return new MemberServiceImpl(new MemoryMemberRepository());
     }
@@ -16,4 +38,5 @@ public class AppConfig {
     public OrderService orderService(){
         return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
     }
+     */
 }
